d9dfd2f858756dbb5fcd5f30e6f99ed7517eb836
OlgaE <olga.efimova.aol@gmail.com>
2013-10-15 12:38:16 +0700

    Using ComboBox to display the content.

--- VaadinSimpleDemo/src/com/example/vaadinsimpledemo/VaadinDemoUI.java
+++ VaadinSimpleDemo/src/com/example/vaadinsimpledemo/VaadinDemoUI.java
@@ -1,13 +1,17 @@
 package com.example.vaadinsimpledemo;
 
+import java.io.File;
+
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.util.FilesystemContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -20,6 +24,13 @@ public class VaadinDemoUI extends UI {
 	@VaadinServletConfiguration(productionMode = false, ui = VaadinDemoUI.class)
 	public static class Servlet extends VaadinServlet {
 	}
+	
+	File file = new File("\\Olga\\tmp");
+	public void check(){	
+		System.out.println(file.isDirectory());
+	}
+	FilesystemContainer docs = new FilesystemContainer(file);
+	ComboBox content = new ComboBox("Documents", docs);
 
 	@Override
 	protected void init(VaadinRequest request) {
@@ -34,6 +45,10 @@ public class VaadinDemoUI extends UI {
 			}
 		});
 		layout.addComponent(button);
+		
+		layout.addComponent(content);
+		VaadinDemoUI ob = new VaadinDemoUI();
+		ob.check();
 	}
 
 }
